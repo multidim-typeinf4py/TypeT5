37293321424cc258afeb7648ede28a970ff6ca58
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-05-28 14:22:55 +0200

    Include experiments

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="TypeT5",
     version="0.1",
-    packages=["typet5"],
+    packages=["typet5", "typet5.experiments"],
     package_dir={"": "src"},
     license="BSD 3-Clause",
 )
