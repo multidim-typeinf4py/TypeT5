bf6df30954caa05b1098d53781d41953e2c93421
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-05-28 15:01:41 +0200

    Include experiments subfolder

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ from distutils import util
 setup(
     name="TypeT5",
     version="0.1",
+    packages=["typet5", "typet5.experiments"],
     package_dir={
-        "": "src",
+        "typet5": util.convert_path("src/typet5"),
         "typet5.experiments": util.convert_path("src/typet5/experiments")
     },
     license="BSD 3-Clause",
